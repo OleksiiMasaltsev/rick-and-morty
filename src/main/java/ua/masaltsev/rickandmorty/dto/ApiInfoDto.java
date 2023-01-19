9504b40a4bf356f57c1766a6a0992482834e3fcb
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2023-01-19 21:42:02 +0200

    dto

---
+++ src/main/java/ua/masaltsev/rickandmorty/dto/ApiInfoDto.java
@@ -0,0 +1,11 @@
+package ua.masaltsev.rickandmorty.dto;
+
+import lombok.Data;
+
+@Data
+public class ApiInfoDto {
+    private int count;
+    private int pages;
+    private String next;
+    private String prev;
+}
