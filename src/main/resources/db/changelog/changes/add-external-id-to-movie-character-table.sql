987307401908576ba0d34dc2af03a44f9d6cb812
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2023-01-26 22:04:50 +0200

    almost works

--- src/main/resources/db/changelog/changes/add-external-id-to-movie-character-table.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
--- changeset <user>:<add-external-id-to-movie-character_table>
-
-ALTER TABLE public.movie_character ADD external_id bigint;
-
--- rollback ALTER TABLE DROP COLUMN external_id;
